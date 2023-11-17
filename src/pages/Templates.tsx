1595419bf873d14a481def49516a72e419ab0f92
Nagy Norbert EGR FIIW43 <norbert.nagy@zf.com>
2023-11-17 10:54:22 +0100

    Templates static style created

--- src/pages/Templates.tsx
+++ src/pages/Templates.tsx
@@ -2,14 +2,23 @@ import Background from '../components/Background';
 import '../assets/styles/App.css';
 import TemplateCard from '../components/TemplateCard';
 import '../assets/styles/Templates.css';
+import { Stack } from '@mui/material';
+import Pagination from '@mui/material/Pagination';
 function Templates() {
-	const testDesc =
-		'Test TestTest TestTest TestTest TestTest TestTest TestTest TestTest TestTest TestTest TestTest Test Test TestTest TestTest TestTest TestTest TestTest TestTest TestTest TestTest TestTest TestTest Test  TestTest Test Test TestTest TestTest TestTest TestTest  TestTest TestTest TestTest TestTest TestTest TestTest TestTest TestTest Test Test TestTest TestTest TestTest TestTest  TestTest TestTest TestTt TestTest ';
+	const testDesc = `Test TestTest TestTest TestTest TestTest TestTest TestTest TestTest TestTest TestTest TestTest Test Test TestTest
+	 					TestTest TestTest TestTest TestTest TestTest TestTest TestTest TestTest TestTest Test  TestTest Test Test 
+	 					TestTest TestTest TestTest TestTest  TestTest TestTest TestTest TestTest TestTest TestTest TestTest 
+	 					TestTest TestTest `;
 	return (
 		<>
 			<div className="content">
 				<div className="content-cards">
 					<TemplateCard title="Test Template" description={testDesc} />
+				</div>
+				<div className="content-pages">
+					<Stack spacing={2}>
+						<Pagination count={2} shape="rounded" />
+					</Stack>
 				</div>
 			</div>
 			<div className="background">
